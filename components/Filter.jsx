8012c695e3e9fced13da40319d7b480b70ab16d1
Thu Dang <>
2023-12-08 13:41:30 -0600

    add addfilter function

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -17,7 +17,14 @@ const Filter = ({ filterTags, setFilterTags }) => {
           <div key={index} className="bg-darkCyan/10 rounded-[5px] inline-flex">
             <p className="py-1 px-2 text-darkCyan text-[13px] leading-[24px] tracking-[-.1px]">{tag}</p>
             <div className="bg-darkCyan w-8 h-8 rounded-r-[5px] grid place-content-center hover:bg-veryDarkGrayishCyan cursor-pointer transition-colors duration-200 ease-in">
-              <Image src={closeIcon} className="w-[14px] h-[14px]" alt={`remove filter ${tag}`} />
+              <Image
+                src={closeIcon}
+                className="w-[14px] h-[14px]"
+                alt={`remove filter ${tag}`}
+                onClick={() => {
+                  setFilterTags(filterTags.filter((item) => item !== tag));
+                }}
+              />
             </div>
           </div>
         ))}
