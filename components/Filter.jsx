4415d5694d58956a71941e246939d3c3ddf5bfb4
Thu Dang <>
2023-12-08 17:54:52 -0600

    adjust filter close button

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -16,16 +16,14 @@ const Filter = ({ filterTags, setFilterTags }) => {
         {filterTags.map((tag, index) => (
           <div key={index} className="bg-darkCyan/10 rounded-[5px] inline-flex">
             <p className="py-1 px-2 text-darkCyan text-[13px] leading-[24px] tracking-[-.1px]">{tag}</p>
-            <div className="bg-darkCyan w-8 h-8 rounded-r-[5px] grid place-content-center hover:bg-veryDarkGrayishCyan cursor-pointer transition-colors duration-200 ease-in">
-              <Image
-                src={closeIcon}
-                className="w-[14px] h-[14px]"
-                alt={`remove filter ${tag}`}
-                onClick={() => {
-                  setFilterTags(filterTags.filter((item) => item !== tag));
-                  console.log(filterTags);
-                }}
-              />
+            <div
+              className="bg-darkCyan w-8 h-8 rounded-r-[5px] grid place-content-center hover:bg-veryDarkGrayishCyan cursor-pointer transition-colors duration-200 ease-in"
+              onClick={() => {
+                setFilterTags(filterTags.filter((item) => item !== tag));
+                console.log(filterTags);
+              }}
+            >
+              <Image src={closeIcon} className="w-[14px] h-[14px]" alt={`remove filter ${tag}`} />
             </div>
           </div>
         ))}
